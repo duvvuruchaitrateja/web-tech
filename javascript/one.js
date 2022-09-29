d2d8ff926f04e0d88624c91e5d9c62c29bc20ede
duvvuruchaitrateja <chaitratejaduvvuru303@gmail.com>
2022-09-29 19:40:22 +0530

    okie

---
+++ javascript/one.js
@@ -0,0 +1,14 @@
+let a = 19
+let b = 3
+let c = 25
+let d = "false"
+let e = "true"
+let f = true
+let g = false
+
+console.log(a + b);
+console.log(c + e);
+console.log(e + d);
+console.log(f + d);
+console.log(c + f);
+console.log(c + g);
